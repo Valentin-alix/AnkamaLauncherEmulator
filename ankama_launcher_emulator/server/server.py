79f14f64bb93ba6b9ba992b6ef5569669b835505
Valentin Alix <valentin-alix.850@outlook.fr>
2024-10-03 23:11:41 +0200

    add time between launch games

--- ankama_launcher_emulator/server/server.py
+++ ankama_launcher_emulator/server/server.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass, field
 from pathlib import Path
 import subprocess
 import sys
-from threading import Thread
+from threading import Lock, Thread
 from time import sleep
 import uuid
 from thrift.transport import TSocket, TTransport
@@ -23,6 +23,7 @@ from ankama_launcher_emulator.gen_zaap.zaap import ZaapService
 class AnkamaLauncherServer:
     handler: AnkamaLauncherHandler
     instance_id: int = field(init=False, default=0)
+    _lock_launch_game: Lock = field(init=False, default_factory=Lock)
 
     def start(self):
         processor = ZaapService.Processor(self.handler)
@@ -34,6 +35,24 @@ class AnkamaLauncherServer:
         thread.start()
 
     def launch_dofus(self, login: str):
+        def _launch_dofus_exe(game_name: str, hash: str):
+            with self._lock_launch_game:
+                subprocess.Popen(
+                    [
+                        DOFUS_PATH,
+                        "--port=26116",
+                        f"--gameName={game_name}",
+                        "--gameRelease=main",
+                        f"--instanceId={self.instance_id}",
+                        f"--hash={hash}",
+                        "--canLogin=true",
+                    ],
+                    stdout=sys.stdout,
+                    stderr=sys.stderr,
+                    text=True,
+                )
+                sleep(1)
+
         print(f"Launch dofus game {login}")
         game_name = "dofus"
         game_id = GAME_ID_BY_NAME[game_name]
@@ -45,22 +64,7 @@ class AnkamaLauncherServer:
         self.handler.infos_by_hash[hash] = AccountGameInfo(
             login, game_id, api_key, Haapi(api_key)
         )
-
-        subprocess.Popen(
-            [
-                DOFUS_PATH,
-                "--port=26116",
-                f"--gameName={game_name}",
-                "--gameRelease=main",
-                f"--instanceId={self.instance_id}",
-                f"--hash={hash}",
-                "--canLogin=true",
-            ],
-            stdout=sys.stdout,
-            stderr=sys.stderr,
-            text=True,
-        )
-        sleep(1)
+        Thread(target=lambda: _launch_dofus_exe(game_name, hash), daemon=True).start()
 
 
 def main():
