c82ae2745086d4809eb36ac888ecb58575b68bd2
Valentin Alix <valentin-alix.850@outlook.fr>
2025-01-25 11:37:48 +0100

    get pid after launched

--- ankama_launcher_emulator/server/server.py
+++ ankama_launcher_emulator/server/server.py
@@ -29,7 +29,6 @@ from ankama_launcher_emulator.gen_zaap.zaap import ZaapService
 class AnkamaLauncherServer:
     handler: AnkamaLauncherHandler
     instance_id: int = field(init=False, default=0)
-    _lock_launch_game: Lock = field(init=False, default_factory=Lock)
     _server_thread: Thread | None = None
     _dofus_threads: list[Thread] = field(init=False, default_factory=list)
 
@@ -41,7 +40,7 @@ class AnkamaLauncherServer:
         server = TServer.TThreadedServer(processor, transport, tfactory, pfactory)
         Thread(target=server.serve, daemon=True).start()
 
-    def launch_dofus(self, login: str):
+    def launch_dofus(self, login: str) -> int:
         print(f"Launch dofus game {login}")
         random_hash = str(uuid.uuid4())
         self.instance_id += 1
@@ -53,13 +52,13 @@ class AnkamaLauncherServer:
             api_key=api_key,
             haapi=Haapi(api_key),
         )
-        Thread(target=lambda: self._launch_dofus_exe(random_hash), daemon=True).start()
+        return self._launch_dofus_exe(random_hash)
 
     def _launch_dofus_exe(
         self,
         random_hash: str,
         config_url: str = OFFICIAL_CONFIG_URL,
-    ):
+    ) -> int:
         log_path = (
             r"C:\Users\valen\AppData\Roaming\zaap\gamesLogs\dofus-dofus3/dofus.log"
         )
@@ -98,19 +97,18 @@ class AnkamaLauncherServer:
             "ZAAP_RELEASE": "dofus3",
         }
 
-        self._launch_exe(command, env)
+        return self._launch_exe(command, env)
 
-    def _launch_exe(self, command: list[str], env: dict[str, Any]):
-        with self._lock_launch_game:
-            subprocess.Popen(
-                command,
-                cwd=Path(DOFUS_PATH).parent,
-                env=nt.environ.copy()
-                | env,  # original env (without converting to uppercase) + custom zaap env
-                start_new_session=True,
-                shell=True,
-            )
-            sleep(1)
+    def _launch_exe(self, command: list[str], env: dict[str, Any]) -> int:
+        process = subprocess.Popen(
+            command,
+            cwd=Path(DOFUS_PATH).parent,
+            env=nt.environ.copy()
+            | env,  # original env (without converting to uppercase) + custom zaap env
+            start_new_session=True,
+            shell=True,
+        )
+        return process.pid
 
 
 def main():
@@ -118,7 +116,7 @@ def main():
     server = AnkamaLauncherServer(handler)
     server.start()
 
-    # server.launch_dofus("ezrealeu44700_1+s1@outlook.com")
+    server.launch_dofus("ezrealeu44700_1+s1@outlook.com")
     server.launch_dofus("ezrealeu44700_2@outlook.com")
 
     while True:
