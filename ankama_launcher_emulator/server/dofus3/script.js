fe2debb44158fbc5b9babfc00898912bf3253fca
Valentin Alix <valentin-alix.850@outlook.fr>
2026-02-22 17:29:04 +0100

    fix frida script dofus3

--- ankama_launcher_emulator/server/dofus3/script.js
+++ ankama_launcher_emulator/server/dofus3/script.js
@@ -2,7 +2,6 @@ const TARGET_PORTS = new Set([5555]);
 
 recv(function (message) {
     const proxyPort = message.port;
-    hookDns();
     hookConnect(proxyPort);
 });
 
@@ -12,37 +11,42 @@ function hookConnect(proxyPort) {
 
     Interceptor.attach(connectPtr, {
         onEnter(args) {
-            const sockaddr = args[1];
-            const family = sockaddr.readU16();
+            try {
+                const sockaddr = args[1];
+                const family = sockaddr.readU16();
 
-            // add(nb octet) permet de déplacer le point à nb d'octet apres sockaddr 
-            if (family === 2) { // IPV4
-                const port = (sockaddr.add(2).readU8() << 8) | sockaddr.add(3).readU8();
+                // add(nb octet) permet de déplacer le point à nb d'octet apres sockaddr 
+                if (family === 2) { // IPV4
+                    const port = (sockaddr.add(2).readU8() << 8) | sockaddr.add(3).readU8();
 
-                if (!TARGET_PORTS.has(port)) return
+                    if (!TARGET_PORTS.has(port)) return
 
-                sockaddr.add(4).writeU32(0x0100007F); // 127.0.0.1
-                sockaddr.add(2).writeU8((proxyPort >> 8) & 0xFF);
-                sockaddr.add(3).writeU8(proxyPort & 0xFF);
+                    sockaddr.add(4).writeU32(0x0100007F); // 127.0.0.1
+                    sockaddr.add(2).writeU8((proxyPort >> 8) & 0xFF);
+                    sockaddr.add(3).writeU8(proxyPort & 0xFF);
 
-            } else if (family === 23) { // IPV6
-                const port =
-                    (sockaddr.add(2).readU8() << 8) |
-                    sockaddr.add(3).readU8();
+                } else if (family === 23) { // IPV6
+                    const port =
+                        (sockaddr.add(2).readU8() << 8) |
+                        sockaddr.add(3).readU8();
 
-                if (!TARGET_PORTS.has(port)) return;
+                    if (!TARGET_PORTS.has(port)) return;
 
-                const ipv6 = sockaddr.add(8);
+                    const ipv6 = sockaddr.add(8);
 
-                ipv6.writeByteArray([
-                    0x00, 0x00, 0x00, 0x00, // 0-3
-                    0x00, 0x00, 0x00, 0x00, // 4-7
-                    0x00, 0x00, 0xFF, 0xFF, // 8-11
-                    0x7F, 0x00, 0x00, 0x01  // 12-15 = 127.0.0.1
-                ]);
+                    ipv6.writeByteArray([
+                        0x00, 0x00, 0x00, 0x00, // 0-3
+                        0x00, 0x00, 0x00, 0x00, // 4-7
+                        0x00, 0x00, 0xFF, 0xFF, // 8-11
+                        0x7F, 0x00, 0x00, 0x01  // 12-15 = 127.0.0.1
+                    ]);
 
-                sockaddr.add(2).writeU8((proxyPort >> 8) & 0xFF);
-                sockaddr.add(3).writeU8(proxyPort & 0xFF);
+                    sockaddr.add(2).writeU8((proxyPort >> 8) & 0xFF);
+                    sockaddr.add(3).writeU8(proxyPort & 0xFF);
+                }
+            }
+            catch (err) {
+                console.info(err.message)
             }
         }
     });
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import logging
 from ankama_launcher_emulator.gui import run_gui
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
-handler.setLevel(logging.DEBUG)
+handler.setLevel(logging.INFO)
 logger.addHandler(handler)
 
 if __name__ in {"__main__", "__mp_main__"}:
